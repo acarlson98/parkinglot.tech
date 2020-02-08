b3e68786cc83c6c656d6a8b2605c397e8e355eee
morganengineer <60806019+morganengineer@users.noreply.github.com>
2020-02-08 09:46:11 -0600

    Update Sonar_2.8.2020.ino
    V2.1 update

--- Mbed/SensorCode/Sonar_2.8.2020/Sonar_2.8.2020.ino
+++ Mbed/SensorCode/Sonar_2.8.2020/Sonar_2.8.2020.ino
@@ -21,16 +21,18 @@
 // NewPing - Version: Latest 
 #include <NewPing.h>
 
-#define RESULT       5
+#define RESULT1      5
+#define RESULT2      6
+#define RESULT3      7
 #define TRIGGER_1    8
 #define ECHO_1       9
-#define TRIGGER_2    12
-#define ECHO_2       13
+#define TRIGGER_2    10
+#define ECHO_2       11
+#define TRIGGER_3    12
+#define ECHO_3       13
 #define MAX_DISTANCE 400
 #define TIMECHECK    5UL
 #define TIMELIMIT    60000UL
-
-NewPing sonar(TRIGGER_1, ECHO_1, MAX_DISTANCE);
 
 float duration, distance;
 int iterations = 10;
@@ -41,17 +43,24 @@ unsigned long startMillis, bigMillis;
 
 void setup() {
   
-    pinMode(RESULT, OUTPUT);
+    pinMode(RESULT1, OUTPUT);
+    pinMode(RESULT2, OUTPUT);
+    pinMode(RESULT3, OUTPUT);
     
-    digitalWrite(RESULT, LOW);
+    digitalWrite(RESULT1, LOW);
+    digitalWrite(RESULT2, LOW);
+    digitalWrite(RESULT3, LOW);
     thisState = LOW;
     bigMillis = 0;
 }
 
 void loop() {
     
+    ////////////////test sensor 1/////////////////
     while(millis() - bigMillis <= TIMELIMIT)
     {
+      NewPing sonar(TRIGGER_1, ECHO_1, MAX_DISTANCE);
+      
       duration = sonar.ping_median(iterations);
     
       //Determine distance from duration
@@ -79,12 +88,55 @@ void loop() {
       }
       else
       {
-        digitalWrite(RESULT, LOW);
+        digitalWrite(RESULT1, LOW);
       }
       
       if ((lastState == HIGH) && (millis() - startMillis >= TIMECHECK))
       {
-      digitalWrite(RESULT, HIGH);
+      digitalWrite(RESULT1, HIGH);
+      }
+    }
+    
+    bigMillis = millis();
+    
+    /////////////////
+    while(millis() - bigMillis <= TIMELIMIT)
+    {
+      NewPing sonar(TRIGGER_1, ECHO_1, MAX_DISTANCE);
+      
+      duration = sonar.ping_median(iterations);
+    
+      //Determine distance from duration
+      // Speed of sound = 343 m/s
+      
+      distance = (duration / 2) * 0.0343;
+      
+      //Send results to Serial Monitor
+      Serial.print("Morgan Galagher. ECEN 1940  Distance = ");
+      if (distance <= 75)
+      {
+        thisState = HIGH;
+      }
+      else
+      {
+        thisState = LOW;
+      }
+      
+      if (lastState != thisState)
+      {
+      //update to the new state
+      lastState = thisState;
+      //record time
+      startMillis = millis();
+      }
+      else
+      {
+        digitalWrite(RESULT1, LOW);
+      }
+      
+      if ((lastState == HIGH) && (millis() - startMillis >= TIMECHECK))
+      {
+      digitalWrite(RESULT1, HIGH);
       }
     }
     
