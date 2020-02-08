caadc6e3bcb8c4a7409066b261024eb52aecb5f6
Takumi Ito <nekotakumi@gmail.com>
2020-02-08 09:34:35 -0600

    Update Esp32-lwmqtt.ino

--- Mbed/Esp32-lwmqtt/Esp32-lwmqtt.ino
+++ Mbed/Esp32-lwmqtt/Esp32-lwmqtt.ino
@@ -13,6 +13,17 @@
  * limitations under the License.
  *****************************************************************************/
 #include "esp32-mqtt.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define LEDPIN 5;
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 char buffer[100];
 bool spot1;
@@ -30,13 +41,39 @@ char checklot(){
     doc["Spot 3"] = spot3;
     doc["Spot 4"] = spot4;
     serializeJson(doc, buffer);
+    
+   //display on the OLED
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    // Display static text
+    display.print("1: ");
+    display.println(spot1);
+    display.print("2: ");
+    display.println(spot2);
+    display.print("3: ");
+    display.println(spot3);
+    display.print("4: ");
+    display.println(spot4);
+    display.display(); 
+    delay(2000);
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEDDPIN, OUTPUT);
   setupCloudIoT();
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Parkinglot.tech");
+  display.display(); 
+  delay(2000);
 }
 
 unsigned long lastMillis = 0;
